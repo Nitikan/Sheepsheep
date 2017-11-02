5f1782b236010d7676a3b1f9b726e20d979b77d9
Nitikan Crwx <nitikan@live.com>
2017-11-02 11:47:21 +0700

    modified space

--- Sheepsheepworld.py
+++ Sheepsheepworld.py
@@ -1,74 +1,41 @@
 import arcade
-
 from models import Sheep,World
-
 from random import randint
 
-
-
-
 SCREEN_WIDTH = 1200
-
 SCREEN_HEIGHT = 800
 
 class ModelSprite(arcade.Sprite):
 
     def __init__(self, *args, **kwargs):
-
         self.model = kwargs.pop('model', None)
-        
- 
-
         super().__init__(*args, **kwargs)
 
- 
-
     def sync_with_model(self):
-
         if self.model:
-
             self.set_position(self.model.x, self.model.y)
-
             self.angle = self.model.angle
 
     def draw(self):
-
         self.sync_with_model()
-
-
-
         super().draw()
 
-
-
 class SpaceGameWindow(arcade.Window):
-
+    
     def __init__(self, width, height):
-
         super().__init__(width, height)
-
- 
-
         arcade.set_background_color(arcade.color.LEMON_MERINGUE)
-
         self.world = World(width,height)
-
         self.sheep_sprite = ModelSprite('images/sheep.png',0.15,model=self.world.sheep)
-
         self.grass_sprite = ModelSprite('images/grass.png',0.1,model=self.world.grass)
-
         self.enemy = []
         for i in self.world.enemy:
              self.enemy.append(ModelSprite('images/wolf.png',0.15,model=i))    
-
         self.bush_sprite = ModelSprite('images/bush.png',0.5,model=self.world.bush)
-
-
 
     def on_key_press(self, key, key_modifiers):
         if self.world.status == 0:
             self.world.on_key_press(key, key_modifiers)
-
 
     def update(self, delta):
         if self.world.status == 0:
@@ -77,27 +44,19 @@ class SpaceGameWindow(arcade.Window):
                 self.enemy.append(ModelSprite('images/wolf.png',0.15,model=i))
             self.world.tmpenemy = []
 
-
     def on_draw(self):
-
         arcade.start_render()
         if(self.world.status == 0):
             self.sheep_sprite.draw()
-
             self.grass_sprite.draw()
-
-
             for i in self.enemy:
                 i.draw()
-
             self.bush_sprite.draw()
             arcade.draw_text(str(self.world.score),self.width - 50, self.height - 50,arcade.color.GRAY, 40)
         if(self.world.status == 1):
             arcade.draw_text("GAME OVER",self.width //2 - 250, self.height //2+20,arcade.color.GRAY, 80)
             arcade.draw_text("score : {}".format(self.world.score),self.width //2 - 80, self.height //2-50,arcade.color.GRAY, 40)
+
 if __name__ == '__main__':
-
-
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-
     arcade.run()
--- models.py
+++ models.py
@@ -1,123 +1,81 @@
 import arcade.key
-
 from random import randint
 
 class Model:
 
     def __init__(self, world, x, y, angle):
-
         self.world = world
-
         self.x = x
-
         self.y = y
-
         self.angle = 0
 
     def hit(self, other, hit_size):
-
         return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 
 class Sheep(Model):
 
     def __init__(self, width, height, x, y):
-
         self.x = x
-        
         self.y = y
-
         self.speedx = 0
-
         self.speedy = 0
-        
         self.width = width
-
         self.height = height
-
         self.angle = 0
 
     def update(self, delta):
-
         self.x += self.speedx
-
         self.y += self.speedy
-
         if self.x > self.width :
-
             self.x = 0
-        
         elif self.x < 0 :
-
             self.x = self.width 
-        
         elif self.y > self.height:
-
             self.y = 0
-
         elif self.y < 0:
-
             self.y = self.height
 
 class Grass(Model):
 
     def __init__(self, world, x, y):
-
         super().__init__(world, x, y, 0)
-
+    
     def random_location(self):
-
         self.x = randint(0, self.world.width - 1)
-
         self.y = randint(0, self.world.height - 1)
 
 class Wolf(Model):
 
     def __init__(self, world, x, y):
-
-        super().__init__(world, x, y, 0)
-        
+        super().__init__(world, x, y, 0)        
         self.speedx = randint(-7,7)
         self.speedy = randint(-7,7)
         self.x = randint(200, 1000)
-
         self.y = randint(200, 600)
 
     def walk(self):
         self.x += self.speedx
-        self.y += self.speedy
-    
+        self.y += self.speedy    
         if self.x > self.world.width or self.x < 0 :
-
             self.speedx *= -1
-        
         if self.y > self.world.height or self.y < 0:
-
             self.speedy *= -1
-
-
 
 class Bush(Model):
 
     def __init__(self, world, x, y):
-
         super().__init__(world, x, y, 0)
 
     def random_location(self):
-
         self.x = randint(0, self.world.width - 1)
-
         self.y = randint(0, self.world.height - 1)
 
 class World:
 
     def __init__(self, width, height):
-
         self.width = width
-
         self.height = height
-
         self.grass= Grass(self, 400, 400)
-
         self.sheep = Sheep(self.width, self.height , 600, 400)
         self.enemy = []
         for i in range(5):
@@ -134,62 +92,43 @@ class World:
     def on_key_press(self, key, key_modifiers):
 
         if key == arcade.key.UP:
-
             self.sheep.speedy = 5
-
             #self.sheep.speedx = 0
-
             self.sheep.angle = -180
         
         elif key == arcade.key.DOWN: 
-    
             self.sheep.speedy = -5
-
             #self.sheep.speedx = 0
-            
             self.sheep.angle = 0
         
         elif key == arcade.key.RIGHT: 
-
             self.sheep.speedx = 5
-
             self.sheep.speedy = 0
-
             self.sheep.angle = 90
         
         elif key == arcade.key.LEFT:
-
             self.sheep.speedx = -5
-
             self.sheep.speedy = 0
-
             self.sheep.angle = -90
         
-
     def update(self, delta):    
         if(self.status == 0):
             self.sheep.update(delta)
             #self.wolf.walk()
             for i in self.enemy:
-                i.walk()
-        
+                i.walk()      
             if self.sheep.hit(self.grass, 15):
-
                 self.grass.random_location()
-
                 self.score += 1
-
             if self.score == self.limitscore:
                 for i in range(1):
                     self.tmp = Wolf(self, 800, 100)
                     self.tmpenemy.append(self.tmp)
                     self.enemy.append(self.tmp)
                 self.limitscore+=2    
-
             for i in self.enemy:
                 if(i.x<self.dotx and i.y <self.doty):
                     i.speedx*=-1
                     i.speedy*=-1
                 if i.hit(self.sheep, 15):
                     self.status = 1
-

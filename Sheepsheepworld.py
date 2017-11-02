5d7314f55e5837f170d73e7ee1bbb1500ad32db5
Nitikan Crwx <nitikan@live.com>
2017-11-02 11:34:50 +0700

    modified gameover

--- Sheepsheepworld.py
+++ Sheepsheepworld.py
@@ -55,7 +55,7 @@ class SpaceGameWindow(arcade.Window):
 
         self.sheep_sprite = ModelSprite('images/sheep.png',0.15,model=self.world.sheep)
 
-        self.grass_sprite = ModelSprite('images/grass.png',0.15,model=self.world.grass)
+        self.grass_sprite = ModelSprite('images/grass.png',0.1,model=self.world.grass)
 
         self.enemy = []
         for i in self.world.enemy:
@@ -66,43 +66,35 @@ class SpaceGameWindow(arcade.Window):
 
 
     def on_key_press(self, key, key_modifiers):
+        if self.world.status == 0:
+            self.world.on_key_press(key, key_modifiers)
 
-        self.world.on_key_press(key, key_modifiers)
 
     def update(self, delta):
-
-        self.world.update(delta)
-        for i in self.world.tmpenemy:
-            self.enemy.append(ModelSprite('images/wolf.png',0.15,model=i))
-        self.world.tmpenemy = []
+        if self.world.status == 0:
+            self.world.update(delta)
+            for i in self.world.tmpenemy:
+                self.enemy.append(ModelSprite('images/wolf.png',0.15,model=i))
+            self.world.tmpenemy = []
 
 
     def on_draw(self):
 
         arcade.start_render()
+        if(self.world.status == 0):
+            self.sheep_sprite.draw()
 
-        self.sheep_sprite.draw()
-
-        self.grass_sprite.draw()
+            self.grass_sprite.draw()
 
 
-        for i in self.enemy:
-            i.draw()
+            for i in self.enemy:
+                i.draw()
 
-        self.bush_sprite.draw()
+            self.bush_sprite.draw()
+            arcade.draw_text(str(self.world.score),self.width - 50, self.height - 50,arcade.color.GRAY, 40)
         if(self.world.status == 1):
-            arcade.draw_text("GAME OVER",
-
-                         self.width //2 - 100, self.height //2,
-
-                         arcade.color.GRAY, 40)
-
-        arcade.draw_text(str(self.world.score),
-
-                         self.width - 50, self.height - 50,
-
-                         arcade.color.GRAY, 40)
-
+            arcade.draw_text("GAME OVER",self.width //2 - 250, self.height //2+20,arcade.color.GRAY, 80)
+            arcade.draw_text("score : {}".format(self.world.score),self.width //2 - 80, self.height //2-50,arcade.color.GRAY, 40)
 if __name__ == '__main__':
 
 
